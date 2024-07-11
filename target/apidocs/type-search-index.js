16cccabdf929df9f0326f836100aca8ebc389954
SeifMohamed55 <seifelden073@gmail.com>
2024-07-11 05:11:48 +0300

    editing docs

--- target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.os.nqueenssolver","l":"ChessBoard"},{"p":"com.os.nqueenssolver","l":"GUIMain"},{"p":"com.os.nqueenssolver","l":"Main"},{"p":"com.os.nqueenssolver","l":"MyTable"},{"p":"com.os.nqueenssolver","l":"NQueensSolver"},{"p":"com.os.nqueenssolver","l":"Utilities"}];updateSearchResults();
